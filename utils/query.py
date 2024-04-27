a3b02cbd66f0093e104d13116f97ffd3136415b1
nabilmuafa <nmuafa@gmail.com>
2024-04-27 10:59:05 +0700

    refactor: rewrite query.py

--- utils/query.py
+++ utils/query.py
@@ -1,3 +1,5 @@
+# Taken and (very) slightly modified from one of last year's TK Basdat
+
 from collections import namedtuple
 import psycopg2
 from psycopg2 import Error
@@ -16,32 +18,30 @@ try:
     connection.autocommit = True
     cursor = connection.cursor()
 except (Exception, Error) as error:
-    print("Error while connecting to PostgreSQL", error)
+    print("Error while connecting to PostgreSQL: ", error)
 
 
 def map_cursor(cursor):
-    "Return all rows from a cursor as a namedtuple"
-    desc = cursor.description
-    nt_result = namedtuple("Result", [col[0] for col in desc])
+    """Return all rows from a cursor as a namedtuple"""
+    description = cursor.description
+    named_tuple_result = namedtuple("Result", [col[0] for col in description])
     return [dict(row) for row in cursor.fetchall()]
 
 
 def query(query_str: str):
-    hasil = []
+    result = []
     with connection.cursor(cursor_factory=RealDictCursor) as cursor:
         cursor.execute("SET search_path TO pacilflix")
         try:
             cursor.execute(query_str)
-
+            # Handling SELECT queries
             if query_str.strip().upper().startswith("SELECT"):
-                # Kalau ga error, return hasil SELECT
-                hasil = map_cursor(cursor)
+                result = map_cursor(cursor)
             else:
-                # Kalau ga error, return jumlah row yang termodifikasi oleh INSERT, UPDATE, DELETE
-                hasil = cursor.rowcount
+                # Returns modified row count
+                result = cursor.rowcount
                 connection.commit()
         except Exception as e:
-            # Ga tau error apa
-            hasil = e
+            result = e
 
-    return hasil
+    return result
