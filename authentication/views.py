92fa0a92b162437308c3772e9690c1ae70460060
nabilmuafa <nmuafa@gmail.com>
2024-04-28 00:42:28 +0700

    add: login impl unfinished

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from utils.query import query
+from django.http import JsonResponse
 
-# Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        data = query(
+            "SELECT username, password FROM pengguna WHERE username = %s AND password = %s", (username, password))
+        print(data)
+        if len(data) == 1:
+            request.session["username"] = username
+            return JsonResponse({
+                "message": "berhasil gan"
+            })
+        else:
+            return JsonResponse({
+                "message": "Gagal gan"
+            })
+    return render(request, "login.html", {})
