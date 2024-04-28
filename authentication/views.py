cca350948b87d24407c7762d32bbcfe0c63dfb23
nabilmuafa <nmuafa@gmail.com>
2024-04-28 13:33:27 +0700

    add: register page

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import redirect, render
 from utils.query import query
 from django.http import JsonResponse
+import random as r
+
+
+def register(request):
+    if "username" in request.session:
+        # TODO: Ganti redirect jadi ke "Shows" (daftar tayangan)
+        return redirect("main:show_main")
+    context = {}
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        negara_asal = request.POST.get("country")
+
+        # Select a random show to recommend to new user
+        random_show = str(r.choice(query("SELECT id FROM tayangan"))["id"])
+
+        # Validate user with the same username doesn't exist
+        exists = query(
+            "SELECT * FROM pengguna WHERE username = %s", (username,))
+        if len(exists) > 0:
+            context["message"] = "Username already taken. Please choose another username."
+        else:
+            data = query("INSERT INTO pengguna (username, password, id_tayangan, negara_asal) VALUES (%s,%s,%s,%s)",
+                         (username, password, random_show, negara_asal))
+            request.session["message"] = "Account creation successful! You can log in now."
+            return redirect("authentication:login")
+    return render(request, "register.html", context)
 
 
 def login(request):
-    if "username" in request.session:
-        return redirect("main:show_main")
     context = {}
+    if "username" in request.session:
+        # TODO: Ganti redirect jadi ke "Shows" (daftar tayangan)
+        return redirect("main:show_main")
+    if "message" in request.session:
+        context["message"] = request.session["message"]
+        del request.session["message"]
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
