d0f6a129e002b046497d11d17368ad2b8896108c
nabilmuafa <nmuafa@gmail.com>
2024-04-28 08:11:29 +0700

    add: logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,21 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from utils.query import query
 from django.http import JsonResponse
 
 
 def login(request):
+    if "username" in request.session:
+        return redirect("main:show_main")
     context = {}
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         data = query(
             "SELECT username, password FROM pengguna WHERE username = %s AND password = %s", (username, password))
-        print(data)
         if len(data) == 1:
             request.session["username"] = username
-            return JsonResponse({
-                "message": "berhasil gan"
-            })
+            # TODO: Ganti redirect jadi ke "Shows" (daftar tayangan)
+            return redirect("main:show_main")
         else:
             context["message"] = "Login failed. Make sure you've inputted the correct credentials."
     return render(request, "login.html", context)
+
+
+def logout(request):
+    del request.session["username"]
+    return redirect("main:show_main")
