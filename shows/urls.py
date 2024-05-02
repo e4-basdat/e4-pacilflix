cd40721124a3963abc023e5dcca0274887c57fde
rapunzzz <kurniathaariq55@gmail.com>
2024-05-02 07:50:41 +0700

    3 Fitur Kelar(sisa design)

--- shows/urls.py
+++ shows/urls.py
@@ -5,4 +5,8 @@ app_name = 'shows'
 urlpatterns = [
     path("", views.show_tayangan, name="tayangan"),
     path('<str:judul>/', views.tayangan_detail, name='detail'),
+    path('<str:judul>/episode/<str:sub_judul>/', views.episode_detail, name='episode'),
+    path('save_review', views.save_review, name='save_review'),
+    path('<str:judul>/update_review/', views.update_review, name='update_review'),
+
 ]
