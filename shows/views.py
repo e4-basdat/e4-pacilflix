8926e409f680ba38f7fa3d20007388490fcb1b9b
rapunzzz <kurniathaariq55@gmail.com>
2024-05-02 16:32:09 +0700

    Add Select country (shows)

--- shows/views.py
+++ shows/views.py
@@ -59,6 +59,44 @@ def show_tayangan(request):
         LIMIT 10;
     """)
     
+    top_ten_lokal = query("""
+        SELECT 
+            TAYANGAN.judul,
+            TAYANGAN.sinopsis_trailer,
+            TAYANGAN.url_video_trailer,
+            to_char(TAYANGAN.release_date_trailer, 'DD-MM-YYYY') as release_date_trailer,
+            COALESCE(total_view_all_time.total_view_all_time, 0) AS total_view_all_time,
+            COALESCE(total_view_7_days.total_view_7_days, 0) AS total_view_7_days
+        FROM 
+            TAYANGAN
+        LEFT JOIN (
+            SELECT 
+                id_tayangan, 
+                COUNT(*) AS total_view_7_days
+            FROM 
+                riwayat_nonton
+            WHERE 
+                end_date_time >= NOW() - INTERVAL '7 days'
+            GROUP BY 
+                id_tayangan
+        ) AS total_view_7_days ON TAYANGAN.id = total_view_7_days.id_tayangan
+        LEFT JOIN (
+            SELECT 
+                id_tayangan, 
+                COUNT(*) AS total_view_all_time
+            FROM 
+                riwayat_nonton
+            GROUP BY 
+                id_tayangan
+        ) AS total_view_all_time ON TAYANGAN.id = total_view_all_time.id_tayangan
+        WHERE
+            TAYANGAN.negara = (SELECT kewarganegaraan FROM contributors LEFT JOIN pengguna p ON p.id WHERE username = "username")
+        ORDER BY 
+            total_view_all_time DESC,
+            TAYANGAN.release_date_trailer DESC
+        LIMIT 10;
+    """)
+    
     film = query("""
         SELECT 
             judul,
