13c51079b36271b4b39cf8486023d4d55e237cab
nabilmuafa <nmuafa@gmail.com>
2024-05-09 21:25:38 +0700

    setting up env. variables for deployment

--- e4_pacilflix/settings.py
+++ e4_pacilflix/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yhqb-*76caon$ho@xnda_&jt^k@yh!%!iwrph-v^2bdeuk77#7'
+SECRET_KEY = os.getenv("SECRET_KEY", "django-secret-key-abcd1234!@#$")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
